f80d0bc27aeaadfccbd23d7c52a57ea58fe064a7
Waachu44 <Waachu44>
2019-04-15 00:27:04 +0200

    dsadsa

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] tab = new int [100];
+            Stopwatch stopwatch = new Stopwatch();
+            for (int i = 0 ; i <= 90; i+=10)
+            {
+
+                tab[i] = i;
+                
+            }
+            foreach (var item in tab)
+
+            {
+                stopwatch.Start();
+                Console.WriteLine(item+" "+stopwatch.ElapsedTicks);
+                stopwatch.Stop();
+                stopwatch.Reset();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
